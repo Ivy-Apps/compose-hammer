f5a2e1c07ecac202ce4928865a98935b6f16978d
iliyangermanov <iliyan.germanov971@gmail.com>
2023-05-26 17:54:37 +0300

    Try to fix a plugin error

--- src/main/kotlin/com/github/ivyapps/composematerial3helper/services/legacy/InsertComponentService.kt
+++ src/main/kotlin/com/github/ivyapps/composematerial3helper/services/legacy/InsertComponentService.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 
 @Service(Service.Level.PROJECT)
@@ -32,15 +33,23 @@ class InsertComponentService(private val project: Project) {
             // Obtain the document instance from the editor
             val document = editor.document
 
+            // PsiDocumentManager instance
+            val psiDocumentManager = PsiDocumentManager.getInstance(project)
+
+            // commit the document
+            psiDocumentManager.commitDocument(document)
+
             // Insert the text at the cursor position
             document.insertString(offset, componentCode)
             reformatService.reformatCode(project, document, offset, componentCode)
             val charsAdded = importsService.addMissingImports(project, file, document, component.imports)
             finalOffset = offset + charsAdded + componentCode.length
+
+            // commit the document after making changes
+            psiDocumentManager.commitDocument(document)
         }
 
         // Optionally, you can move the cursor to the end of the inserted text
         caretModel.moveToOffset(finalOffset)
     }
-
 }
