e24bd9c8bda76cda9bcdf7640cec84ed12a6aaac
iliyangermanov <iliyan.germanov971@gmail.com>
2023-05-28 18:29:28 +0300

    Add search bars

--- src/main/kotlin/com/ivyapps/composematerial3helper/content/SearchBarsContent.kt
+++ src/main/kotlin/com/ivyapps/composematerial3helper/content/SearchBarsContent.kt
@@ -129,4 +129,129 @@ fun ContentScope.searchBars() = group("Search bars") {
             }
         """.trimIndent()
     }
+
+    component {
+        name = "Docked search bar"
+        specUrl = "https://m3.material.io/components/search/specs"
+        guidelinesUrl = "https://m3.material.io/components/search/guidelines"
+        docsUrl =
+            "https://developer.android.com/reference/kotlin/androidx/compose/material3/package-summary#DockedSearchBar(kotlin.String,kotlin.Function1,kotlin.Function1,kotlin.Boolean,kotlin.Function1,androidx.compose.ui.Modifier,kotlin.Boolean,kotlin.Function0,kotlin.Function0,kotlin.Function0,androidx.compose.ui.graphics.Shape,androidx.compose.material3.SearchBarColors,androidx.compose.ui.unit.Dp,androidx.compose.foundation.interaction.MutableInteractionSource,kotlin.Function1)"
+        screenshot = "searchbar_docked"
+        description = """
+            A search bar represents a floating search field that allows users to enter a keyword or phrase and get relevant information. It can be used as a way to navigate through an app via search queries.
+            An active search bar expands into a search "view" and can be used to display dynamic suggestions.
+            A DockedSearchBar displays search results in a bounded table below the input field. 
+            It is meant to be an alternative to SearchBar when expanding to full-screen size is undesirable on large screens such as tablets.
+        """.trimIndent()
+        imports = listOf(
+            "androidx.compose.material.icons.Icons",
+            "androidx.compose.material.icons.filled.MoreVert",
+            "androidx.compose.material.icons.filled.Search",
+            "androidx.compose.material3.DockedSearchBar",
+            "androidx.compose.material3.ExperimentalMaterial3Api",
+            "androidx.compose.material3.Icon",
+            "androidx.compose.material3.Text",
+            "androidx.compose.runtime.Composable",
+            "androidx.compose.runtime.getValue",
+            "androidx.compose.runtime.mutableStateOf",
+            "androidx.compose.runtime.saveable.rememberSaveable",
+            "androidx.compose.runtime.setValue",
+            "androidx.compose.ui.Modifier",
+        )
+        code = """
+            var text by rememberSaveable { mutableStateOf("") }
+            var active by rememberSaveable { mutableStateOf(false) }
+    
+            DockedSearchBar(
+                modifier = Modifier,
+                query = text,
+                onQueryChange = { text = it },
+                onSearch = { active = false },
+                active = active,
+                onActiveChange = { active = it },
+                placeholder = { Text("Hinted search text") },
+                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) }
+            ) {
+                // Add search query suggestions here
+            }
+        """.trimIndent()
+        customCode = """
+            var text by rememberSaveable { mutableStateOf("") }
+            var active by rememberSaveable { mutableStateOf(false) }
+    
+            Box(Modifier.fillMaxSize()) {
+                Box(
+                    Modifier
+                        .zIndex(1f)
+                        .fillMaxWidth()
+                ) {
+                    DockedSearchBar(
+                        modifier = Modifier
+                            .align(Alignment.TopCenter)
+                            .padding(top = 8.dp),
+                        query = text,
+                        onQueryChange = { text = it },
+                        onSearch = { active = false },
+                        active = active,
+                        onActiveChange = { active = it },
+                        placeholder = { Text("Hinted search text") },
+                        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                        trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+                        colors = SearchBarDefaults.colors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            dividerColor = MaterialTheme.colorScheme.primary,
+                            inputFieldColors = TextFieldDefaults.colors()
+                        )
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentPadding = PaddingValues(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(4.dp)
+                        ) {
+                            items(4) { idx ->
+                                val resultText = "Suggestion N"
+    
+                                ListItem(
+                                    headlineContent = { Text(resultText) },
+                                    supportingContent = { Text("Additional info") },
+                                    leadingContent = {
+                                        Icon(
+                                            Icons.Filled.Star,
+                                            contentDescription = null
+                                        )
+                                    },
+                                    modifier = Modifier.clickable {
+                                        text = resultText
+                                        active = false
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+    
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        top = 72.dp,
+                        end = 16.dp,
+                        bottom = 16.dp
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    val list = List(100) { "Text N" }
+    
+                    items(count = list.size) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            text = list[it]
+                        )
+                    }
+                }
+            }
+        """.trimIndent()
+    }
 }
